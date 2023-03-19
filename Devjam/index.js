af2f8938e1e5c35345d9376a9a4f654760235623
Lakshit Rajput <110688132+Luxit5490@users.noreply.github.com>
2023-03-19 12:58:13 +0530

    Update index.js

--- Devjam/index.js
+++ Devjam/index.js
@@ -61,3 +61,45 @@ function anima2()  {
 
 secelm.addEventListener('mouseover',anima); //Adding the events to the function anima
 secelm.addEventListener('mouseout', anima2); //Adding the events to the function anima2
+
+
+
+
+function search(){
+    let que = document.getElementById('searchtext').value.toLowerCase();
+    var eleme = document.getElementsByClassName('card');
+    let arr=[];
+    let x = 0;
+    let arr2=[];
+    for(var k=0;k<eleme.length;k++){
+        arr.push(eleme[k].id);
+    }
+
+    for(var m=0;m<arr.length;m++){
+        if (que==""){
+            window.location = "index.html";
+        }
+        else if (arr[m].indexOf(que) > -1){
+            let arr2 = document.getElementsByClassName('card')
+            for (var n=0;n<arr2.length;n++){
+                arr2[n].style.display = 'none';
+            }
+
+            document.getElementById('moviesmore').style.display='none';
+            document.getElementById(arr[m]).style.display="";
+            x=1
+        }
+    }
+    if ((x==0) && (que!="")){
+        window.alert('Movie/Series not found');
+    }
+}
+
+function yourFunctionName(event) {
+    if (event.keyCode == 13) {
+       search()
+       return true;
+    } else {
+       return true;
+    }
+ }
